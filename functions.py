db5f72622fb28f23992ad03f5c0d39438c832811
Morgan <m.garricks@lancaster.ac.uk>
2024-11-01 17:34:41 +0000

    makes a call to an API that converts city names to their coordinates then makes a call to another API to fetch weather data from those coordinates

---
+++ functions.py
@@ -0,0 +1,23 @@
+import os
+import requests
+
+API_KEY = os.getenv("WEATHERAPIKEY")
+
+def get_coords(place):
+    url = f"https://api.openweathermap.org/geo/1.0/direct?q={place}&limit={1}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    lat = data[0]["lat"]
+    long = data[0]["lon"]
+    return lat, long
+
+def get_data(place,forecast_days=None,category=None):
+    lat,lon = get_coords(place)
+    url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place="london"))
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import plotly.express as px
+from functions import get_data
+
 st.title("weather forecast for the next days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -7,10 +9,7 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-dates=["2022-25-10","2022-26-10","2022-27-10","2022-28-10",]
-temperatures = [10,11,12,13]
-temperatures = [days*i for i in temperatures]
-
+dates,temperatures = get_data(place,days,option)
 figure = px.line(x=dates,y=temperatures,labels={"x": "Date", "y": "Temperature (C)"})
 st.plotly_chart(figure)
 
