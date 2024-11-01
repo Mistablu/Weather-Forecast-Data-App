8ff61777cf5215ad6469d8b36ccf83f90b5c777a
Morgan <m.garricks@lancaster.ac.uk>
2024-11-01 19:44:15 +0000

    display sky data and correct temperature data based on sliders, handle incorrect location errors, convert to celsius

--- functions.py
+++ functions.py
@@ -11,20 +11,15 @@ def get_coords(place):
     long = data[0]["lon"]
     return lat, long
 
-def get_data(place,forecast_days,category):
+def get_data(place,forecast_days):
     lat,lon = get_coords(place)
     url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     filtered_data = filtered_data[:(8*forecast_days)]
-
-    if category == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if category == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="london",forecast_days=2,category="Sky"))
+    print(get_data(place="london",forecast_days=2))
--- main.py
+++ main.py
@@ -9,7 +9,23 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-dates,temperatures = get_data(place,days,option)
-figure = px.line(x=dates,y=temperatures,labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+if place:
+    try:
+        data = get_data(place,days)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in data]
+            dates = [dict["dt_txt"] for dict in data]
+            figure = px.line(x=dates,y=temperatures,labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                    "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            conditions = [dict["weather"][0]["main"] for dict in data]
+            image_paths = [images[condition] for condition in conditions]
+            st.image(image_paths,width=115)
+    except IndexError:
+        st.write("This city does not exist")
+        
+
 
